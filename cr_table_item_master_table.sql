908ec07e31517476fdbcb276bcb73fee471a9899
milind-brahme <milind.brahme@outlook.com>
2021-12-27 09:07:49 +0530

    some change

---
+++ cr_table_item_master_table.sql
@@ -0,0 +1,6 @@
+create table item_master_table(
+item_number varchar2(40),
+item_desc varchar2(200),
+item_uom varchar2(10),
+item_type varchar2(20),
+constraint item_master_table_pk primary key (item_number));
